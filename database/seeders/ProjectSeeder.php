fa46f64390e3021219de3989b629c0d8e80b8281
silviosalza <silviofilipposalza@gmail.com>
2023-06-05 20:11:54 +0200

    seeder &faker added

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 20; $i++) { 
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->slug = Str::slug($project->title,'-');
+            $project->content = $faker->text();
+            $project->save();
+        }
     }
 }

6abf9345e03750538b09e49155feb152fdc8d718
silviosalza <silviofilipposalza@gmail.com>
2023-06-05 19:29:13 +0200

    migration added but not working

--- database/migrations/2023_06_05_151250_create_projects_table.php
+++ database/migrations/2023_06_05_151250_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('content')->nullable();
+            $table->string('slug');
+            $table->string('img')->nullable();
             $table->timestamps();
         });
     }

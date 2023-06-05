4ce6f1f3915c81805326033751f2d56cfa801854
silviosalza <silviofilipposalza@gmail.com>
2023-06-05 20:35:02 +0200

    projects table printed

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::all();
-        dd($projects);
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>I miei progetti</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th scope="row">{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td>
+                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
+                            <i class="fa-solid fa-eye"></i>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+@endsection

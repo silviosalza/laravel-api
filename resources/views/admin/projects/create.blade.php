5a14d5058995ce8b8f9d38b5f6ba3d2bec79b03f
silviosalza <silviofilipposalza@gmail.com>
2023-06-06 13:12:26 +0200

    form create added

---
+++ resources/views/admin/projects/create.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('content')
+{{-- @include('partials.errors') --}}
+
+    <form action="{{ route('admin.projects.store') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+        </div>
+        <div class="mb-3">
+            <label for="img" class="form-label">Url immagine</label>
+            <input type="text" class="form-control" id="img" name="img" value="{{ old('img') }}">
+        </div>
+        <div class="mb-3">
+            <label for="content" class="form-label">Contenuto</label>
+            <textarea class="form-control" id="content" rows="3" name="content">{{ old('content') }}</textarea>
+        </div>
+        <button class="btn btn-primary" type="submit">Crea</button>
+    </form>
+
+@endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,30 +3,4 @@
 @section('content')
     <h1>Progetto {{ $project->title }}</h1>
     <p>{{ $project->content }}</p>
-
-    {{-- <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Titolo</th>
-                <th scope="col">Slug</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($projects as $project)
-                <tr>
-                    <th scope="row">{{ $project->id }}</th>
-                    <td>{{ $project->title }}</td>
-                    <td>{{ $project->slug }}</td>
-                    <td>
-                        <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
-                            <i class="fa-solid fa-eye"></i>
-                        </a>
-                    </td>
-                </tr>
-            @endforeach
-
-        </tbody>
-    </table> --}}
 @endsection

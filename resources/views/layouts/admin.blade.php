bfdff15cdeda5f57e33f1b07fc778ecc47d8d939
silviosalza <silviofilipposalza@gmail.com>
2023-06-06 12:42:17 +0200

    added routes in admin.blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -64,8 +64,8 @@
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.posts.index' ? 'bg-secondary' : '' }}"
-                                    href=>
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'admin.projects.index' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">
                                     <i class="fa-regular fa-folder-open"></i> Projects
                                 </a>
                             </li>
